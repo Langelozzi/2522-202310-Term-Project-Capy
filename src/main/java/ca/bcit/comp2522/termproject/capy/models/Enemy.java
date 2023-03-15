8859fcfa7022857922347a348cabe60862166c86
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2023-03-14 17:37:36 -0700

    create enemy class

---
+++ src/main/java/ca/bcit/comp2522/termproject/capy/models/Enemy.java
@@ -0,0 +1,21 @@
+package ca.bcit.comp2522.termproject.capy.models;
+
+import javafx.scene.image.Image;
+
+public class Enemy extends Character {
+    private final int ATTACK_MULTIPLIER = 3;
+    private final int difficulty;
+    private final int attackDamage;
+    // private SugarCane sugarCane;
+
+    Enemy(final int difficulty) {
+        super(new Image("file:src/main/resources/ca/bcit/comp2522/termproject/capy/sprites/crocodile.png"));
+        this.difficulty = difficulty;
+        this.attackDamage = ATTACK_MULTIPLIER * difficulty;
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
