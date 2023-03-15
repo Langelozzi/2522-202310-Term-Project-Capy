3e6bd5ebc8d6857882d3652b5c290b14e138d935
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2023-03-14 17:39:11 -0700

    update module-info.java to open controllers to fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,4 +10,5 @@ module ca.bcit.comp2522.termproject.capy {
     exports ca.bcit.comp2522.termproject.capy;
     exports ca.bcit.comp2522.termproject.capy.models;
     opens ca.bcit.comp2522.termproject.capy.models to javafx.fxml;
+    opens ca.bcit.comp2522.termproject.capy.controllers to javafx.fxml;
 }
