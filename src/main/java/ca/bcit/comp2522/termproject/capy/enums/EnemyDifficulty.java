cc57939b96405f455c9264c1da9e4445749ea16d
Langelozzi <lucasangelozzi32@gmail.com>
2023-04-05 11:40:38 -0700

    Add javadocs for new enum

--- src/main/java/ca/bcit/comp2522/termproject/capy/enums/EnemyDifficulty.java
+++ src/main/java/ca/bcit/comp2522/termproject/capy/enums/EnemyDifficulty.java
@@ -1,5 +1,10 @@
 package ca.bcit.comp2522.termproject.capy.enums;
 
+/**
+ * EnemyDifficulty enum contains the 3 difficulties that an enemy could be.
+ * @author COMP2522 Group 13
+ * @version 1.0.0
+ */
 public enum EnemyDifficulty {
     EASY,
     MEDIUM,
